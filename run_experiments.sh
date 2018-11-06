04fb14d227cc7e4bfe0bd7bbbccc344ca823d6a7
August Shi <awshi2@illinois.edu>
2018-11-06 10:59:11 -0600

    Adding checking logic

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-octave example1.m
-octave tables.m
+octave example1.m > computed_results.txt
+octave tables.m >> computed_results.txt
+
+echo "CHECKING DIFFERENCES BETWEEN EXPECTED AND COMPUTED"
+diff results.txt computed_results.txt
