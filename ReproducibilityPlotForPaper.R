d5d62afd26d745e384b57ce72d969aac8c45d167
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-02-21 14:39:16 -0600

    Add script to produce plot for paper

---
+++ ReproducibilityPlotForPaper.R
@@ -0,0 +1,5 @@
+reproducibility_data = read.csv("./reproducibility.dat")
+png("ReproducibilityPlotForPaper.png", height=2.5, width=5, units="in", res=300)
+par(mar=c(2,2,1,1))
+plot(reproducibility_data$Time, reproducibility_data$Percentage, 's', ylim=c(0,100), xlab="", ylab="")
+dev.off()
