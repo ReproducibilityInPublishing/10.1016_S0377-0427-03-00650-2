9bb27470fc558386c38bdc4716b49ed571e63657
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-04-10 16:24:28 -0500

    Change python2 to plain python. mistake from before.

--- computation.sh
+++ computation.sh
@@ -4,4 +4,4 @@ octave run_example1.m > computed_results.txt
 octave run_tables.m >> computed_results.txt
 
 echo "CHECKING DIFFERENCES BETWEEN EXPECTED AND COMPUTED"
-python2 check_results.py expected_results.txt computed_results.txt
+python check_results.py expected_results.txt computed_results.txt
