ac2726a1d85c9eb8debbe3ea191ca3a7059a19f5
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-04-15 10:24:19 -0500

    Remove scripting redundancy.

--- computation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-octave run_example1.m > computed_results.txt
-octave run_tables.m >> computed_results.txt
-
-echo "CHECKING DIFFERENCES BETWEEN EXPECTED AND COMPUTED"
-python check_results.py expected_results.txt computed_results.txt
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./computation.sh
+octave run_example1.m > computed_results.txt
+octave run_tables.m >> computed_results.txt
+
+echo "CHECKING DIFFERENCES BETWEEN EXPECTED AND COMPUTED"
+python check_results.py expected_results.txt computed_results.txt
