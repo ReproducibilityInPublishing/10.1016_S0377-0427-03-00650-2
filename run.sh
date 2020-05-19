4d1a8384054b54bf964ac97f7e33c84f355abf39
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-05-19 08:51:50 -0500

    Move expected output into the expected_output directory

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 octave run_tests.m > computed_results.txt
 
 echo "CHECKING DIFFERENCES BETWEEN EXPECTED AND COMPUTED"
-python2 check_results.py expected_results.txt computed_results.txt
+python2 check_results.py expected_output/expected_results.txt computed_results.txt
